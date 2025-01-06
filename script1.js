a7c8a18550c4d138c5814b42fc230fa9f310a944
uswelcomeu <uswelcomeu@gmail.com>
2025-01-07 00:58:26 +0800

    2025010700582174

--- script1.js
+++ script1.js
@@ -30,7 +30,7 @@
                 }
             })
         };
-        handle({ currPath: `C:\\xxx\\2025010700465594` });
+        handle({ currPath: `C:\\xxx\\2025010700582174` });
         handle({ currPath: `C:\\xxx\\20250101` });
     }
 })();

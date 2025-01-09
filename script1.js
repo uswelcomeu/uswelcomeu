46c2203a4a958df8c9a4b1fd45b8c7abc46687d7
uswelcomeu <uswelcomeu@gmail.com>
2025-01-09 17:00:39 +0800

    2025010917003467

--- script1.js
+++ script1.js
@@ -30,7 +30,7 @@
                 }
             })
         };
-        handle({ currPath: `C:\\xxx\\2025010916592189` });
+        handle({ currPath: `C:\\xxx\\2025010917003467` });
         handle({ currPath: `C:\\xxx\\20250101` });
     }
 })();
